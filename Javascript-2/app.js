017247b0ee78b503fed4070a582ffcb24a0fb0c6
Yonerfy <34351998+Yonerfy@users.noreply.github.com>
2021-02-23 21:10:24 +0100

    Add files via upload

---
+++ Javascript-2/app.js
@@ -0,0 +1,6 @@
+console.log("Hallo Winc Academy studenten");
+let multiplication = 1230941 * 1823794;
+let division = 637263 / 54;
+console.log(multiplication);
+console.log(division);
+// "Dit is een grote som".
