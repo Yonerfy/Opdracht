73f5fa97c368c94a62f0eb0f76adf54a0d1dc341
Yonerfy <34351998+Yonerfy@users.noreply.github.com>
2021-02-26 21:23:31 +0100

    Add files via upload

---
+++ Javascript-6/app.js
@@ -0,0 +1,30 @@
+const howToMakePizza = function () {
+    console.log("Take the dough out and roll it into a circle");
+    console.log("Puff out the edge of the dough so the crust is thicker at the edge");
+    console.log("Pre-heat oven to 250-260 degrees C. (Remember to put a pizza stone in the oven)");
+    console.log("Put on the pizza sauce, cheese, and toppings.");
+    console.log("Put the pizza on the pizza stone in the oven.");
+    console.log("Bake for 30 minutes or until pizza is crispy and golden brown. (make sure you don't burn the toppings)");
+    console.log("Try putting the toppings on the pizza differently to make the pizza look cool! (you can also change the shape of your pizza such as square or triangle)");
+    console.log("Enjoy your homemade pizza!!!");
+}
+// because we are not calling function
+howToMakePizza();
+howToMakePizza();
+
+
+
+const howToMakeSushi = function () {
+    console.log("Prep the Rice & Nori");
+    console.log("Spread the Rice on the Nori");
+    console.log("Add Fillings to the Bottom Quarter of the Nori");
+    console.log("Roll the Sushi Part Way");
+    console.log("Roll the Sushi the Rest of the Way");
+    console.log("Move to Cutting Board");
+    console.log("Cut the Sushi");
+    console.log("Serve");
+}
+
+howToMakeSushi();
+howToMakeSushi();
+howToMakeSushi();
